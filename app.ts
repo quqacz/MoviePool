82288eeda1d5db18d0aa4eafe89ca525e29ef594
quqacz <67021265+quqacz@users.noreply.github.com>
2021-10-29 12:21:37 +0200

    db connetcion

--- app.ts
+++ app.ts
@@ -1,8 +1,28 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
 import express, { Request, Response} from "express";
 import { requestLogger } from "./middleware/requestLogger";
 
-const app = express();
-const port = 8000;
+const port = process.env.PORT || 3000;
+
+const mongoose = require('mongoose')
+
+const app = express()
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/MoviePool'
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+db.on("error", console.error.bind(console, 'connection error:'))
+db.once("open", ()=>{
+    console.log("Database connected")
+})
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");

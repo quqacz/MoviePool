f38e6017ac983499d41327158a687837e8ec0d2c
quqacz <67021265+quqacz@users.noreply.github.com>
2021-10-29 17:48:35 +0200

    user model

--- app.ts
+++ app.ts
@@ -5,14 +5,22 @@ if(process.env.NODE_ENV !== 'production'){
 import express, { Request, Response} from "express";
 import { requestLogger } from "./middleware/requestLogger";
 
-const port = process.env.PORT || 3000;
-
 const mongoose = require('mongoose')
-
 const app = express()
+const session = require('express-session')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const methodOverride = require('method-override')
 
+// db models
+
+const User = require('./models/user')
+
+// .env constants
+const port = process.env.PORT || 3000;
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/MoviePool'
 
+// db setup
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -24,10 +32,37 @@ db.once("open", ()=>{
     console.log("Database connected")
 })
 
+// express setup
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
+// sesion config
+const secret = process.env.SECRET || 'thisisagoodsecretforfuckssake';
+
+const sessionConfig = {
+    secret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig))
+
+// passport setup
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// routes
 app.get('/', requestLogger, (req: Request, res: Response)=>{
     res.render('index');
 })
@@ -37,6 +72,12 @@ app.get('/login', (req: Request, res: Response)=>{
 })
 
 app.get('/register', (req: Request, res: Response)=>{
+    res.render('register')
+})
+
+app.post('/register', (req: Request, res: Response)=>{
+    const { name, lastName } = req.body;
+    console.log(name, lastName);
     res.render('register')
 })
 

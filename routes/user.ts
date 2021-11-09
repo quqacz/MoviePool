7afb77afad3fd38e298ecd72e947cf22ff113ac6
quqacz <67021265+quqacz@users.noreply.github.com>
2021-11-09 13:32:50 +0100

    very not finished friend list

--- routes/user.ts
+++ routes/user.ts
@@ -1,11 +1,41 @@
 import express, {Request, Response, NextFunction } from 'express'
+import { use } from 'passport';
 const User = require('../models/user')
 const Users = express()
 
 Users.get('/:id', async (req: Request, res: Response)=>{
-    const user = await User.find({_id: req.params.id}).populate('friends').populate('polls');
-    console.log(user.friends.length, user.polls.length)
-    res.render('userProfile', {user})
+    try{
+        const user = await User.findOne({_id: req.params.id}).populate('friends').populate('polls');
+        res.render('userProfile', {user, friend: undefined})
+    }catch(e){
+        console.log(e)
+        res.redirect('/');
+    }
+})
+
+Users.post('/:id/find', async (req: Request, res: Response)=>{
+    try{
+        const { friendName } = req.body
+        const user = await User.findOne({_id: req.params.id}).populate('friends').populate('polls');
+        const friend = await User.findOne({nickname: friendName})
+        res.render('userProfile', {user, friend})
+    }catch(e){
+        console.log(e)
+        res.redirect('/');
+    }
+})
+
+Users.post('/:id/add/:friendId', async (req: Request, res: Response)=>{
+    try{
+        const user = await User.findOne({_id: req.params.id}).populate('friends').populate('polls');
+        const friend = await User.findOne({_id: req.params.friendId})
+        user.friends.push(friend);
+        user.save();
+        res.redirect('/user/'+user._id)
+    }catch(e){
+        console.log(e)
+        res.redirect('/');
+    }
 })
 
 export default Users
